44dd342159c35a8d313174cac4440f3b5a4a851f
Julio Silva <julio@juliobs.com>
2012-09-29 17:01:30 -0300

    Usando stdio para subir no ranking

--- URI/01177-preenchimento_de_vetor_ii.cpp
+++ URI/01177-preenchimento_de_vetor_ii.cpp
@@ -4,27 +4,24 @@
 // 
 //    Description:  URI 1177 - Preenchimento de Vetor II
 // 
-//        Version:  1.0
+//        Version:  1.1
 //        Created:  09/28/2012 03:38:35 PM
-//       Revision:  none
+//       Revision:  Troquei iostream por cstdio para conseguir 0.00s
 //       Compiler:  g++
 // 
 //         Author:  Julio B. Silva (351202), julio(at)juliobs.com
 //        Company:  UFSCar
 // 
 // ============================================================================
-#include <iostream>
-
-using namespace std;
+#include <cstdio>
 
 int main()
 {
     int t;
-    cin >> t;
+    scanf("%d", &t);
 
     for (int i=0; i < 1000; i++)
-    {
-        cout << "N[" << i << "] = " << i%t << endl;
-    }
+        printf("N[%d] = %d\n", i, i%t);
+
     return 0;
 }
