936080a585e0eaa43170775457816d53a9eedc43
Julio Silva <julio@juliobs.com>
2013-02-15 20:41:45 -0200

    URI 1209 - RTE

--- URI/01209-festas_de_sao_petersburgo.cpp
+++ URI/01209-festas_de_sao_petersburgo.cpp
@@ -4,7 +4,7 @@
 // 
 //    Description:  URI 1209 - Festas de São Petersburgo
 // 
-//        Version:  1.0 - TLE
+//        Version:  2.0 - Runtime Error
 //        Created:  14/Feb/2013 19:26:40
 //       Revision:  none
 //       Compiler:  g++
@@ -14,17 +14,30 @@
 // 
 // ============================================================================
 #include <cstdio>
-#include <set>
-using namespace std;
+#include <cstring>
+
+int pessoas, relacoes, min_amigos, p1, p2;
+bool g[1001][1001];
+int amigos[1001];
+
+inline void remover(int i)
+{
+    amigos[i] = 0;
+
+    for (int j = 1; j <= pessoas; j++)
+        if (g[i][j])
+        {
+            g[i][j] = g[j][i] = 0;
+            amigos[j]--;
+
+            if (amigos[j] && amigos[j] < min_amigos)
+                remover(j);
+        }
+}
 
 int main()
 {
-    int pessoas, relacoes, p1, p2;
-    unsigned int min_amigos;
-    set<int> g[1001];
-    set<int>::iterator it;
-
-    while (scanf("%d %d %u", &pessoas, &relacoes, &min_amigos) != EOF)
+    while (scanf("%d %d %d", &pessoas, &relacoes, &min_amigos) != EOF)
     {
         if (min_amigos == 0)
         {
@@ -38,35 +51,25 @@ int main()
         }
         else
         {
+            memset(g, 0, sizeof(g));
+            memset(amigos, 0, sizeof(amigos));
+
             for (int i = 0; i < relacoes; i++)
             {
                 scanf("%d %d", &p1, &p2);
-                g[p1].insert(p2);
-                g[p2].insert(p1);
+                g[p1][p2] = g[p2][p1] = 1;
+                amigos[p1]++;
+                amigos[p2]++;
             }
 
-            bool forever_alones = true; // Pessoas com menos de min_amigos
-            while (forever_alones)
-            {
-                forever_alones = false;
-
-                for (int i = 1; i <= pessoas; i++)
-                    if (!g[i].empty() && g[i].size() < min_amigos)
-                    {
-                        forever_alones = true;
-
-                        for (it = g[i].begin(); it != g[i].end(); ++it)
-                            g[*it].erase(i);
-
-                        g[i].clear();
-                    }
-            }
+            for (int i = 1; i <= pessoas; i++)
+                if (amigos[i] && amigos[i] < min_amigos)
+                    remover(i);
 
             // Imprime todos os que sobraram
             bool imprimiu = false;
-
             for (int i = 1; i <= pessoas; i++)
-                if (!g[i].empty())
+                if (amigos[i])
                 {
                     if (!imprimiu)
                     {
@@ -75,9 +78,6 @@ int main()
                     }
                     else
                         printf(" %d", i);
-
-                    // Limpa set para próximo input
-                    g[i].clear();
                 }
 
             if (imprimiu)
