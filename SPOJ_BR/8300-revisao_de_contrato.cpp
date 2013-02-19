5f767a56656a655e3ee3a058fecc4327c92b440a
Julio Silva <julio@juliobs.com>
2013-02-19 18:56:25 -0300

    SPOJ BR 8300 corrigido

--- SPOJ_BR/8300-revisao_de_contrato.cpp
+++ SPOJ_BR/8300-revisao_de_contrato.cpp
@@ -1,12 +1,20 @@
 // ============================================================================
 // 
 //       Filename:  8300-revisao_de_contrato.cpp (SPOJ BR)
+//                  11830-contract_revision.cpp (UVa)
 //                  01120-revisao_de_contrato.cpp (URI)
 // 
 //    Description:  SPOJ BR 8300 - Revisão de contrato
+//                  UVa 11830 - Contract Revision
+//                  URI 1120 - Revisão de contrato
+//
+//                  Primeira fase da Maratona de Programação - 2010
 //                  Resolvido durante o primeiro simulado do grupo do Facebook
+//
+//                  Estranhamente, um código errado (Version 1.0) passou no
+//                  SPOJ e no UVa.
 // 
-//        Version:  1.0
+//        Version:  2.0
 //        Created:  08/12/2012 03:01:38 PM
 //       Revision:  none
 //       Compiler:  g++
@@ -27,16 +35,16 @@ int main()
     
     while (cin >> d >> n && d != '0')
     {
-        bool imprimiu = false;
-        for (i=0; i<n.length(); i++)
+        bool imprimiu = false, imprimir_zero = false;
+        for (i = 0; i < (int)n.length(); i++)
         {
-            bool imprimir_zero = false;
             if (n.at(i) != d)
             {
                 if (n.at(i) != '0' && n.at(i) != d)
                     imprimir_zero = true;
 
-                if (i==n.length()-1 && n.at(i) == '0')
+                // Se o último caractere é zero, imprima o zero
+                if (i == (int)n.length()-1 && n.at(i) == '0')
                     imprimir_zero = true;
 
                 if (n.at(i) != '0')
