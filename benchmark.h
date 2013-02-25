32388ee1a2eecd0f7214b393d3c16884f3148c29
Julio Silva <julio@juliobs.com>
2013-02-24 21:26:07 -0300

    retab

--- benchmark.h
+++ benchmark.h
@@ -25,16 +25,16 @@
 #include <ctime>
 timespec diff(timespec start, timespec end)
 {
-	timespec temp;
-	if (end.tv_nsec - start.tv_nsec < 0)
+    timespec temp;
+    if (end.tv_nsec - start.tv_nsec < 0)
     {
-		temp.tv_sec = end.tv_sec - start.tv_sec - 1;
-		temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
-	}
+        temp.tv_sec = end.tv_sec - start.tv_sec - 1;
+        temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
+    }
     else
     {
-		temp.tv_sec = end.tv_sec - start.tv_sec;
-		temp.tv_nsec = end.tv_nsec - start.tv_nsec;
-	}
-	return temp;
+        temp.tv_sec = end.tv_sec - start.tv_sec;
+        temp.tv_nsec = end.tv_nsec - start.tv_nsec;
+    }
+    return temp;
 }
