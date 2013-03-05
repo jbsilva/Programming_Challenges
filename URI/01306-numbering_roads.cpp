b8de5e58d14494e3bf6858be9dcc8f54274e1e1d
Julio Silva <julio@juliobs.com>
2013-03-05 13:06:49 -0300

    URI 1306 -> UVa 11723 resolvido

---
+++ URI/01306-numbering_roads.cpp
@@ -0,0 +1,1 @@
+../UVa/11723-numbering_roads.cpp
---
+++ UVa/11723-numbering_roads.cpp
@@ -0,0 +1,39 @@
+// ============================================================================
+// 
+//       Filename:  11723-numbering_roads.cpp (UVa)
+//                  01306-numbering_roads.cpp (URI)
+// 
+//    Description:  UVa 11723 - Numbering Roads
+//                  URI 1306 - Numbering Roads
+// 
+//        Version:  1.0
+//        Created:  05/Mar/2013 12:31:14
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Julio Batista Silva (351202), julio(at)juliobs.com
+//        Company:  UFSCar
+// 
+// ============================================================================
+#include <cstdio>
+
+int main()
+{
+    int R, N, d, caso=0;
+
+    while (scanf("%d %d", &R, &N) && (R || N))
+    {
+        printf("Case %d: ", ++caso);
+        d = (R + N - 1) / N - 1; // d = ceil(R/N) - 1
+
+        if (d > 26)
+            puts("impossible");
+        else if (d <= 0)
+            puts("0");
+        else
+            printf("%d\n", d);
+    }
+
+    return 0;
+}
+
