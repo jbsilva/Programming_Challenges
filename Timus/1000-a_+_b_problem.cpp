b59a68c54a2b789a506c605321553748e97f4389
Julio Silva <julio@juliobs.com>
2013-07-04 19:38:36 -0300

    Novo juiz: ACM Timus

---
+++ Timus/1000-a_+_b_problem.cpp
@@ -0,0 +1,25 @@
+// ============================================================================
+//
+//       Filename:  1000-a_+_b_problem.cpp
+//
+//    Description:  Timus 1000 - A + B Problem
+//
+//        Version:  1.0
+//        Created:  01/Jul/2013 00:37:35
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Julio Batista Silva (351202), julio(at)juliobs.com
+//        Company:  UFSCar
+//
+// ============================================================================
+#include <cstdio>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a + b);
+    return 0;
+}
+
